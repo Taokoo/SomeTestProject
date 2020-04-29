0dbe6a7d98e4c3cb63bae42d1cec9ef241af3cba
taokoo <one@taokoo.com>
2020-04-29 13:25:38 +0800

    .

---
+++ BPM_Flowable/src/main/java/com/taokoo/SpringApplications.java
@@ -0,0 +1,11 @@
+package com.taokoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplications {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApplications.class, args);
+	}
+}
