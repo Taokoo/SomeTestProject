522a7c23e4d3aa02c635c8ddf4cc5016465deb3e
taokoo <one@taokoo.com>
2020-04-30 14:18:58 +0800

    更新代码

--- BPM_Flowable/src/main/java/com/taokoo/controller/TestLeaveDemo.java
+++ BPM_Flowable/src/main/java/com/taokoo/controller/TestLeaveDemo.java
@@ -5,10 +5,12 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.flowable.bpmn.model.BpmnModel;
+import org.flowable.engine.HistoryService;
 import org.flowable.engine.ProcessEngine;
 import org.flowable.engine.ProcessEngineConfiguration;
 import org.flowable.engine.RepositoryService;
@@ -19,7 +21,11 @@ import org.flowable.engine.runtime.ProcessInstance;
 import org.flowable.image.ProcessDiagramGenerator;
 import org.flowable.task.api.Task;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -34,6 +40,8 @@ public class TestLeaveDemo {
     private RepositoryService repositoryService;
     @Autowired
     private ProcessEngine processEngine;
+    @Autowired
+    private HistoryService historyService;
     
     /**
      * 发起一个流程
@@ -68,32 +76,37 @@ public class TestLeaveDemo {
     /**
      * 批准
      * @param taskId 任务ID
+     * @param assignee 审核人（编号）
      * @return
      */
     @RequestMapping(value = "apply")
-    public String apply(String taskId) {
+    public String apply(@RequestParam("taskId")String taskId,@RequestParam("assignee")String assignee,@RequestBody Map<String, Object> map) {
         Task task = taskService.createTaskQuery().taskId(taskId).singleResult();
         if (task == null) {
             throw new RuntimeException("流程不存在");
         }
+        taskService.setAssignee(taskId, assignee);//设置审核人
+        // 设置任务参数，也可不设置：key value，只是示例
+        // 带 Local 为局部参数，只适用于本任务，不带 Local 为全局任务，可在其他任务调用参数
+        taskService.setVariableLocal(taskId, "status", true);
         //通过审核
-        HashMap<String, Object> map = new HashMap<>();
         map.put("outcome", "通过");
         taskService.complete(taskId, map);
-        return "processed ok!";
+        return "申请通过";
     }
     
     /**
      * 拒绝
      * @param taskId
+     * @param assignee 审核人（编号）
      * @return
      */
     @RequestMapping(value = "reject")
-    public String reject(String taskId) {
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("outcome", "驳回");
+    public String reject(@RequestParam("taskId")String taskId,@RequestParam("assignee")String assignee,@RequestBody Map<String, Object> map) {
+    	taskService.setAssignee(taskId, assignee);//设置审核人
+    	map.put("outcome", "驳回");
         taskService.complete(taskId, map);
-        return "reject";
+        return "申请驳回";
     } 
     
     /**
@@ -147,5 +160,22 @@ public class TestLeaveDemo {
                 out.close();
             }
         }
-    } 
+    }
+    
+    /**
+     * 通过流程实例 Id，判断流程是否结束
+     * @param processInstanceId 流程实例 Id
+     * @return true 结束，false 未结束
+     */
+    @RequestMapping(value = "/checkProcessInstanceFinish/{processInstanceId}", method = RequestMethod.GET)
+    public boolean checkProcessInstanceFinish(@PathVariable(value = "processInstanceId") String processInstanceId) {
+        boolean isFinish = false;
+        // 根据流程 ID 获取未完成的流程中是否存在此流程
+        long count = historyService.createHistoricProcessInstanceQuery().unfinished().processInstanceId(processInstanceId).count();
+        // 不存在说明没有结束
+        if (count == 0) {
+            isFinish = true;
+        }
+        return isFinish;
+    }
 }
