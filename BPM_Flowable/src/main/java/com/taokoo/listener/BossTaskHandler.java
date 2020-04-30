578af619a91d54574fb4c0667b6de4015bdf9b9d
小明哥儿 <64536762+Xiaomingger@users.noreply.github.com>
2020-04-30 22:22:07 +0800

    add mailUtil class

--- BPM_Flowable/src/main/java/com/taokoo/listener/BossTaskHandler.java
+++ BPM_Flowable/src/main/java/com/taokoo/listener/BossTaskHandler.java
@@ -3,11 +3,14 @@ package com.taokoo.listener;
 import org.flowable.engine.delegate.TaskListener;
 import org.flowable.task.service.delegate.DelegateTask;
 
+import com.taokoo.util.MailUtil1;
+
 public class BossTaskHandler implements TaskListener {
 
 	@Override
 	public void notify(DelegateTask delegateTask) {
 		System.out.println("老板审批...");
+		MailUtil1.sendMail("ocean@taokoo.com", "审批邮件", "老板已审批通过");
 		delegateTask.setAssignee("老板");
 	}
 }
