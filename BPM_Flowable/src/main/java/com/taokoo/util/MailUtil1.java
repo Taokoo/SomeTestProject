6abb39ebe49fbd07235c532ee5174e02dc9c88d5
taokoo <one@taokoo.com>
2020-04-30 13:15:43 +0800

    update

--- BPM_Flowable/src/main/java/com/taokoo/util/MailUtil1.java
+++ BPM_Flowable/src/main/java/com/taokoo/util/MailUtil1.java
@@ -1,5 +1,9 @@
 package com.taokoo.util;
-
+/**
+ * 邮件工具类
+ * @author 朱启明
+ *
+ */
 public class MailUtil1 {
 
 }
--- BPM_Flowable/src/main/java/com/taokoo/util/MailUtil2.java
+++ BPM_Flowable/src/main/java/com/taokoo/util/MailUtil2.java
@@ -1,5 +1,10 @@
 package com.taokoo.util;
 
+/**
+ * 邮件工具类
+ * @author 小七
+ *
+ */
 public class MailUtil2 {
 
 }
